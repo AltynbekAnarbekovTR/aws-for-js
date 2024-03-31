15cf26306a1244e3a3093a8ef851688e7e7c8029
Altynbek Anarbekov <altynbek_anarbekov@epam.com>
2024-04-01 02:46:55 +0600

    finish task 2

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to the project
+
+## CloudFront URL: https://d3gtakupyusafp.cloudfront.net/
+
+## S3-website: http://storemanual.s3-website-us-east-1.amazonaws.com/
+
+aws-cdk folder contains package.json with necessary scripts
--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,16 +1,46 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketAccessControl,
+} from "aws-cdk-lib/aws-s3";
+import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a new S3 bucket for storing the static site content
+    const bucket = new Bucket(this, "SiteBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Deploy site contents to S3 bucket
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("../react-shop/dist")],
+      destinationBucket: bucket,
+    });
+
+    // Create a CloudFront distribution to serve your website globally
+    new CloudFrontWebDistribution(this, "Distribution", {
+      defaultRootObject: "index.html",
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+    });
   }
 }
+
+// Example usage
+const app = new cdk.App();
+new AwsCdkStack(app, "MyStaticSiteStack");
